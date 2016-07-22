17408d0d6144577ae0854aabaa36d1a6061f0773
massat <masato.hirai@gmail.com>
2016-07-22 22:18:02 +0900

    Initial commit.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'docker'
+require 'serverspec'
+
+set :backend, :docker
+set :docker_url, ENV['DOCKER_HOST']
+
+Excon.defaults[:ssl_verify_peer] = false
---
+++ spec/ubuntu/base_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+set :docker_image, 'massat/ubuntu-base'
+
+describe 'base' do
+end
